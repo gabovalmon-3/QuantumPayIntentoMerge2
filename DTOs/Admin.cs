ec5f3957e862d1a24471733153d98f67e2c12f7f
DiegoCS06 <dcalvos@ucenfotec.ac.cr>
2025-06-27 16:00:48 -0600

    Add project files.

---
+++ DTOs/Admin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs
+{
+    public class Admin : BaseDTO
+    {
+        public string nombreUsuario { get; set; }
+        public string contrasena { get; set; }
+    }
+}
---
+++ DTOs/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs
+{
+    public class Cliente : BaseDTO
+    {
+        public string cedula { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public int telefono { get; set; }
+        public string direccion { get; set; }
+        public string fotoCedula { get; set; }
+        public DateOnly fechaNacimiento { get; set; }
+        public string fotoPerfil { get; set; }
+        public string contrasena { get; set; }
+        public string IBAN { get; set; }
+    }
+}
