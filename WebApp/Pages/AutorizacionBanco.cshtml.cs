7b57444e17de38ad02dcb31d4903612f1ac49da3
Selenaquir95 <squiross@ucenfotec.ac.cr>
2025-07-18 18:45:30 -0600

    web ui new pages added

---
+++ WebApp/Pages/AutorizacionBanco.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages
+{
+    public class AutorizacionBancoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
