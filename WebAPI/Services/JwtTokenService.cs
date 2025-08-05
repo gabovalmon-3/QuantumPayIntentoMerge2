3bd9e9cb6a6b03ca4b5f4c184d5135463d481080
gabovalmon-3 <gabovalmon@icloud.com>
2025-08-05 11:22:04 -0600

    .

--- WebAPI/Services/JwtTokenService.cs
+++ WebAPI/Services/JwtTokenService.cs
@@ -1,7 +1,8 @@
-﻿using System.IdentityModel.Tokens.Jwt;
+﻿using Amazon.Rekognition.Model;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
-using Microsoft.IdentityModel.Tokens;
 
 namespace WebAPI.Services
 {
